197427d93ae71ec6d4a4283decf55c7d55bc62ec
lorainchan <zundae02@gmail.com>
2025-11-13 13:03:41 +0800

    Add files via upload

---
+++ Covid19Dashboard.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import plotly_express as px
+
+@st.cache_data()
+
+def load_data():
+    url="https://raw.githubusercontent.com/datasets/covid-19/main/data/countriesaggregated.csv"
+    
+    df=pd.read_csv(url)
+    df['Date']=pd.to_datetime(df['Date'])
+    
+    return df
+
+df=load_data()
+st.sidebar.header("Filter Options")    
+country=st.sidebar.selectbox("Select Country", df['Country'].unique())
+metric=st.sidebar.radio("Select Metric", ['Confirmed', 'Recovered', 'Deaths'])
+filtered_df=df[df['Country']==country]
+fig=px.line(filtered_df, x='Date', y=metric, title=f"{metric}Cases in {country}")
+st.plotly_chart(fig, use_container_width=True)
+if st.checkbox("Show Raw Data"):
+    st.write(filtered_df)
